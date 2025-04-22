6f318d25b35307b73b2430dedab75cdb86b58327
Watanabe Issei <c0a2427878@edu.teu.ac.jp>
2025-04-22 15:10:47 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,6 +48,10 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
